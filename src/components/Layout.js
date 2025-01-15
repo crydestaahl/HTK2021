13cc0f0776642184f6e669d1acb85d4e497fd557
Christoffer Rydeståhl <kristoffer@oholics.com>
2025-01-15 15:51:45 +0100

    added gtm

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,59 +1,68 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import './all.sass'
-import useSiteMetadata from './SiteMetadata'
-import { withPrefix } from 'gatsby'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import './all.sass';
+import useSiteMetadata from './SiteMetadata';
+import { withPrefix } from 'gatsby';
 
 const TemplateWrapper = ({ children }) => {
-  const { title, description } = useSiteMetadata()
+  const { title, description } = useSiteMetadata();
   return (
     <div>
       <Helmet>
-        <html lang="en" />
+        <html lang='en' />
         <title>{title}</title>
-        <meta name="description" content={description} />
+        <meta name='description' content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
+          rel='apple-touch-icon'
+          sizes='180x180'
           href={`${withPrefix('/')}img/favicon-16x16.png`}
         />
         <link
-          rel="icon"
-          type="image/png"
+          rel='icon'
+          type='image/png'
           href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
+          sizes='32x32'
         />
         <link
-          rel="icon"
-          type="image/png"
+          rel='icon'
+          type='image/png'
           href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
+          sizes='16x16'
         />
 
         <link
-          rel="mask-icon"
+          rel='mask-icon'
           href={`${withPrefix('/')}img/favicon-32x32.png`}
         />
-        <meta name="theme-color" content="#b71020" />
+        <meta name='theme-color' content='#b71020' />
 
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
+        <meta property='og:type' content='business.business' />
+        <meta property='og:title' content={title} />
+        <meta property='og:url' content='/' />
         <meta
-          property="og:image"
+          property='og:image'
           content={`${withPrefix('/')}img/og-image.jpg`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5NZGSL2L');
+          `,
+          }}
         />
       </Helmet>
       <Navbar />
-      <div>
-          {children}
-        </div>    
-      <Footer />      
+      <div>{children}</div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
