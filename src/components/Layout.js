d11cf48e680379e5298e5953a7b456c543f0d6d6
Christoffer Rydeståhl <kristoffer@oholics.com>
2025-01-19 15:05:26 +0100

    added gtm

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -46,18 +46,24 @@ const TemplateWrapper = ({ children }) => {
           property='og:image'
           content={`${withPrefix('/')}img/og-image.jpg`}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+        <script>
+          {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-5NZGSL2L');
-          `,
-          }}
-        />
+          `}
+        </script>
       </Helmet>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5NZGSL2L"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
       <Navbar />
       <div>{children}</div>
       <Footer />
